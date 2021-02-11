26f4bb6f158ab3bf3b2dbd70bd6c55f70601c459
Stackbit Code Editor <support@stackbit.com>
2021-02-11 11:19:44 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,7 +32,7 @@ class MyDocument extends Document {
 
     render() {
         return (
-            <html lang=IT
+            <html lang=IT></html>
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
                 <body {...this.helmetBodyAttrComponents}>
